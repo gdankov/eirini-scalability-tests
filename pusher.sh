4c8c8b160194b4e9c17f7b90cf7ce81df8e671e7
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2019-05-23 18:11:11 +0300

    Update pusher script
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- pusher.sh
+++ pusher.sh
@@ -1,55 +1,52 @@
 #!/bin/bash
 
-readonly COMMAND="${1:?Provide a command}"
-readonly TIMES="${2:?How many times?}"
+set -e
+
+readonly TIMES="${1:?How many times?}"
 readonly DOMAIN="158.176.93.40.nip.io"
 readonly CURL_COUNT="5"
 readonly TIME_BETWEEN_CURLS="1"
 
-FAIL=0
+readonly APP_NAME="staticfile"
+readonly LOGS_DIR="../logs"
+
 
 push-it() {
-    cf push -m "256M" "staticfile${1}" >"../logs/staticfile${1}" 2>&1
-}
-
-delete() {
-    for i in $(seq 1 "$TIMES");do cf delete -f "staticfile${i}"; done
+    local current_app_name="${APP_NAME}${1}"
+    cf push -m "256M" "${current_app_name}" >"${LOGS_DIR}/${current_app_name}" 2>&1
 }
 
 curl-it() {
-    echo -e "\n\n================== CURLING TIME ==================\n\n" >> "../logs/staticfile${1}"
-    local url="staticfile${1}.${DOMAIN}"
+    local app_index="$1"
+    local url="${APP_NAME}${1}.${DOMAIN}"
+
+    echo -e "\n\n================== CURLING TIME ==================\n\n" >> "${LOGS_DIR}/${APP_NAME}${1}"
     for i in $(seq 1 "$CURL_COUNT");do
-        curl --fail "$url" >> "../logs/staticfile{1}" 2>&1
+        curl --fail "$url" >> "${LOGS_DIR}/${APP_NAME}${app_index}" 2>&1
         if [ $? -eq 0 ]; then
-            echo -e "Try ${i}: success\n" >> "../logs/staticfile${1}"
+            echo -e "Try ${i}: success\n" >> "${LOGS_DIR}/${APP_NAME}${app_index}"
         else
-            echo -e "Try ${i}: failure\n" >> "../logs/staticfile${1}"
-            FAIL=1
+            echo -e "Try ${i}: failure\n" >> "${LOGS_DIR}/${APP_NAME}${app_index}"
         fi
         sleep "$TIME_BETWEEN_CURLS"
     done
 }
 
-
-test() {
+test-it() {
     local index="$1"
-    touch "../logs/staticfile${index}"
+    mkdir -p "$LOGS_DIR"
+    touch "${LOGS_DIR}/${APP_NAME}${index}"
     push-it "$index"
     curl-it "$index"
 }
 
 main() {
-    if [ "$COMMAND" == "clean" ];then
-        delete
-    elif [ "$COMMAND" == "push" ];then
-        cd staticfile
-        for i in $(seq 1 $TIMES);do
-            test "$i" &
-        done
-        wait
-        exit $FAIL
-    fi
+    cd staticfile
+    for i in $(seq 1 $TIMES);do
+        test-it "$i" &
+        # sleep 0.5
+    done
+    wait
 }
 
 main
